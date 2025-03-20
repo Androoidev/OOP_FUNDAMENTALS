f6f0fddf752f9d1d122ea12c19829df904ba6b6c
Androoidev <luiszenteno.dev@gmail.com>
2025-03-20 02:02:49 -0300

    feat: add basic banking functionality with deposit and withdrawal features
    - Implemented deposit and withdrawal methods in the BankAccount class.
    - Added menu system for user interaction to deposit, withdraw or exit.
    - Included balance checks and validations for deposits and withdrawals.

---
+++ src/main/kotlin/fundamentals/BankAccount.kt
@@ -0,0 +1,76 @@
+package fundamentals
+
+fun main(){
+
+    val account1 = BankAccount(10.0,"Luis")
+
+    account1.setBalance()
+
+}
+class BankAccount(private var balance: Double, private val holder: String) {
+
+    fun getBalance() : Double{
+        return balance
+    }
+
+    fun setBalance(){
+        var cond = true
+
+        while (cond) {
+            println(
+                """
+            |  BANK ACCOUNT
+            |  ------------------
+            |  1. DEPOSIT MONEY
+            |  2. WITHDRAW MONEY
+            |  3. EXIT
+            |  ------------------
+            """.trimMargin()
+            )
+            print("PLEASE CHOOSE AN OPTION (1-3): ")
+            val opt = readlnOrNull()?.toIntOrNull()
+
+            when (opt) {
+                1 -> depositMoney()
+                2 -> withdrawMoney()
+                3 -> {
+                    println("EXITING...")
+                    cond = false
+                }
+                else -> println("INVALID OPTION. PLEASE TRY AGAIN")
+            }
+        }
+
+
+
+    }
+
+    fun depositMoney(){
+        print("ENTER THE AMOUNT TO DEPOSIT: ")
+        val amount = readlnOrNull()?.toDoubleOrNull()
+
+        if (amount != null && amount > 0) {
+            balance += amount
+            println("YOU HAVE ADDED $$amount. AVAILABLE BALANCE: $$balance")
+        } else {
+            println("INVALID AMOUNT. MUST BE MORE THAN $0")
+        }
+    }
+
+    fun withdrawMoney(){
+        print("ENTER THE AMOUNT TO WITHDRAW: ")
+        val amount = readlnOrNull()?.toDoubleOrNull()
+
+        if (amount != null && amount > 0){
+            if (amount <= balance){
+                balance -= amount
+                println("YOU HAVE WITHDRAWED $$amount. REMAINING BALANCE: $$balance ")
+            } else {
+                println("INSUFFICIENT FUNDS TO WITHDRAW THIS AMOUNT")
+            }
+        } else {
+            println("INVALID AMOUNT. MUST BE OVER $0")
+        }
+
+    }
+}
