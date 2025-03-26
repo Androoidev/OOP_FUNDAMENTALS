edcb2b0d628208abe999270ca5bac893188b158a
Androoidev <luiszenteno.dev@gmail.com>
2025-03-26 13:55:37 -0300

    feat(customers): implement shopping cart functionality in Customer class >> >> - Added 'addToCart(product: Product)' method to allow customers to add products to their shopping cart. >> - Implemented 'getCartTotal()' to calculate the total price of products, including discounts. >> - Created 'checkout()' method to display the cart total and simulate the payment process.

--- src/main/kotlin/inheritance/product/Customer.kt
+++ src/main/kotlin/inheritance/product/Customer.kt
@@ -1,16 +1,22 @@
 package inheritance.product
 
-class Customer(val clientName: String, val email: String, val shoppingCart : MutableList<String>){
+class Customer(val clientName: String, val email: String, val shoppingCart : MutableList<Product> = mutableListOf()) {
 
-    fun addToCart(product: Product){
-
+    fun addToCart(product: Product) {
+        shoppingCart.add(product)
+        println("THE PRODUCT ${product.name} HAS BEEN ADDED TO SHOPPING CART SUCCESSFULLY")
     }
 
-    fun getCartTotal(){
-
+    fun getCartTotal(): Double {
+        return shoppingCart.sumOf { it.price }
     }
 
-    fun chekout(){
-
+    fun checkout() {
+        val total = getCartTotal()
+        println("TOTAL: $$total")
+        println("Processing payment...")
+        if (total != 0.0){
+            println("Payment successfully")
+        }
     }
 }
