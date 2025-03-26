523f441eac7c135839973ae74ab0b65f21c56d04
Androoidev <luiszenteno.dev@gmail.com>
2025-03-26 13:59:12 -0300

    chore(main): add instances and method calls for testing
    - Created instances of Product and Customer to simulate real usage.
    - Added calls to 'addToCart()', 'getCartTotal()', and 'checkout()' for testing purposes.
    - Ensured basic functionality of the shopping cart system is executed in 'Main.kt'.
    - No changes to business logic, only added sample data and method calls for demonstration.

--- src/main/kotlin/inheritance/product/Main.kt
+++ src/main/kotlin/inheritance/product/Main.kt
@@ -1,12 +1,24 @@
 package inheritance.product
 
 fun main(){
+
     val product1 = Product("Ropa", 100.0)
+    val product2 = Product("Polera", 230.0)
+
     product1.applyDiscount(20.0)
 
     val cloth = Clothing("Shirt", 40.0, "Large", "Red")
-    println(cloth.getClothingInfo())
+    println(cloth.getProductInfo())
 
     val electronics = Electronics("TV", 140.30, "Samsung", 12)
-    println(electronics.getElectronicsInfo())
+    println(electronics.getProductInfo())
+
+    val customer = Customer("Luis", "asjd@gmail.com", mutableListOf())
+    customer.addToCart(product1)
+    customer.addToCart(product2)
+
+    println(customer.getCartTotal())
+
+    customer.checkout()
 }
+
